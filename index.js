3e4365db5727cc6e5e5e5ea7194046dad90b9702
Dhruv-i023 <121281466+Dhruv-i023@users.noreply.github.com>
2023-08-02 02:20:22 +0530

    index.js

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ function showAns(a)
     var x = document.querySelectorAll(".answer")[a];
     var s = document.querySelectorAll("svg")[a];
     
-    if (x.style.display === "none") {
-        for(var i=0; i<6; i++){
-            var y = document.querySelectorAll(".answer")[i];
-            y.style.display = "none";
+
+    for(var i=0; i<6; i++){
+        var y = document.querySelectorAll(".answer")[i];
+        var s2 = document.querySelectorAll("svg")[i];
+        if(y==x){
+            continue;
         }
-        x.style.display = "block";
-    } 
-    else {
-        x.style.display = "none";
+        if(y.style.height!='0px'){
+            s2.style.transform='rotate(0deg)';
+        }
+        y.style.height="0px";
+        y.style.padding="0px";
+    }
+    if(x.style.height==="0px"){
+        s.style.transform = 'rotate(45deg)';
+        x.style.height=x.scrollHeight+20+"px";
+        x.style.padding="15px";
+    }
+    else{
+        s.style.transform = 'rotate(0deg)';
+        x.style.padding="0px";
+        x.style.height="0px";
     }
 }
